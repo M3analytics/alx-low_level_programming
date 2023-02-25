229d0c43f0767c044deaecd0a63c5af170e684ca
M3analytics <morganmuema@gmail.com>
2023-02-25 08:14:51 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,7 +4,6 @@
  * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n, m, 1;
@@ -13,14 +12,14 @@ int main(void)
 	{
 		for (m = 49; m < 58 ; m++)
 		{
-			for (1 = 50; 1 <58; 1++)
+			for (1 = 50; 1 < 58; 1++)
 			{
 				if (1 > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(1);
-					if (n != 55 || m !=56)
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
