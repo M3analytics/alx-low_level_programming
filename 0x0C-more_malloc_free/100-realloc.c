ee70770e7e4a9b9c9bb304d9916a7fcb697252b1
M3analytics <morganmuema@gmail.com>
2023-03-17 10:05:37 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,7 +9,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *relloc;
+	char *clone, *realloc;
 	unsigned int i;
 
 	if (ptr != NULL)
@@ -25,15 +25,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr)
 			return (0);
 	}
-	relloc = malloc(new_size);
-	if (relloc == NULL)
+	realloc = malloc(new_size);
+	if (realloc == NULL)
 		return (0);
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		*(relloc + i) = clone[i];
+		*(realloc + i) = clone[i];
 	}
 	free(ptr);
-	return (relloc);
+	return (realloc);
 }
 
 
